27f7e5d932b1b850fc47204a2dd49c8ceefc46d3
Sebastian Ramos <sebastian.ramos70@myhunter.cuny.edu>
2025-07-09 18:28:18 +0000

    added register to the navbar for testing

--- flask_app.py
+++ flask_app.py
@@ -28,7 +28,7 @@ def register():
 @app.route("/update_server", methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('/home/thealienseb/SEO_Flask_practice')
+        repo = git.Repo('/TheAlienSeb/SEO_Flask_practice')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
